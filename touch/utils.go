c6b8dd1f5e9149deb4779cfafc3133c34937354e
esha.maharishi <esha.maharishi@viam.com>
2025-12-16 16:05:19 -0500

    fix copy-paste error

--- touch/utils.go
+++ touch/utils.go
@@ -319,7 +319,7 @@ func GetMergedPointCloud(ctx context.Context, positions []toggleswitch.Switch, s
 
     if traceID != "" {
         // Save merged pcd
-		if err := file_utils.SavePointCloudFile(mergedPc, "merged.pcd", traceID, time.Now()); err != nil {
+		if err := file_utils.SavePointCloudFile(big, "merged.pcd", traceID, time.Now()); err != nil {
 			return nil, err
 		}
     }
